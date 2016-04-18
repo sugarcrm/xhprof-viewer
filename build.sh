abb929aa0904fa24fcdd692be2dbc40a3a70dd7f
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2016-04-18 14:49:29 +0200

    Extend build script to assemble an rpm package

--- build.sh
+++ build.sh
@@ -50,6 +50,54 @@ sed -i.bak "s/VIEWER_VERSION/$npm_package_version/g" \
 rm $package_name/config.php.bak
 
 tar -czf "../$package_name.tgz" $package_name
-cd ..
+
+# building rpm package
+mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS,tmp}
+cd rpmbuild
+cp ../../$package_name.tgz SOURCES/
+
+cat <<EOF > SPECS/$package_name.spec
+# Spec file for $package_name
+
+%define _topdir `pwd`
+%define name $npm_package_name
+%define version $npm_package_version
+%define release 1
+%define buildroot %{_topdir}/%{name}-%{version}-root
+
+Summary: SugarCRM XHProf Viewer
+Name: %{name}
+Version: %{version}
+Release: %{release}
+License: SugarCRM Licence
+Group: Development/Tools
+Source: %{name}-%{version}.tgz
+URL: https://github.com/sugarcrm/xhprof-viewer
+BuildRoot: %{buildroot}
+BuildArch: noarch
+
+%description
+%{summary}
+
+%prep
+%setup -q
+
+%build
+# Nothing to build
+
+%install
+mkdir -p \$RPM_BUILD_ROOT/var/www/html/xhp
+cp -R ./* \$RPM_BUILD_ROOT/var/www/html/xhp
+
+%files
+/var/www/html/xhp
+
+EOF
+
+rpmbuild -bb --target=noarch --target=noarch-unknown-linux SPECS/$package_name.spec
+mv RPMS/noarch/$package_name-1.noarch.rpm ../../
+
+cd ../..
 rm -r dist
 ls $package_name.tgz
+ls $package_name-1.noarch.rpm
