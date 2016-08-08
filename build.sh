928e7605a3f00bb247b22a9a932aed644fca807d
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2016-08-08 10:14:20 +0200

    Enable travis

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 #
 # © 2016 SugarCRM Inc.  Licensed by SugarCRM under the Apache 2.0 license.
 #
@@ -17,8 +17,9 @@ if [ -a 'dist' ]; then
 fi;
 
 package_name=$npm_package_name-$npm_package_version
-if [ -a "$package_name.tgz" ]; then
-    rm $package_name.tgz
+archive_name=$npm_package_name
+if [ -a "$archive_name.tar.gz" ]; then
+    rm $archive_name.tar.gz
 fi;
 
 mkdir -p dist/$package_name
@@ -30,6 +31,10 @@ rsync -Rr --exclude=.DS_Store \
  templates \
  index.php \
  README.md \
+ LICENSE \
+ CONTRIBUTING.md \
+ CONTRIBUTOR_TERMS.pdf \
+ THIRD_PARTY_LICENSES.pdf \
  bower_components/jquery/dist/jquery.min.js \
  bower_components/bootstrap/dist/css/bootstrap.min.css \
  bower_components/bootstrap/dist/js/bootstrap.min.js \
@@ -51,55 +56,5 @@ sed -i.bak "s/VIEWER_VERSION/$npm_package_version/g" \
  $package_name/config.php
 rm $package_name/config.php.bak
 
-tar -czf "../$package_name.tgz" $package_name
-
-# building rpm package
-mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS,tmp}
-cd rpmbuild
-cp ../../$package_name.tgz SOURCES/
-
-cat <<EOF > SPECS/$package_name.spec
-# Spec file for $package_name
-
-%define _topdir `pwd`
-%define name $npm_package_name
-%define version $npm_package_version
-%define release 1
-%define buildroot %{_topdir}/%{name}-%{version}-root
-
-Summary: SugarCRM XHProf Viewer
-Name: %{name}
-Version: %{version}
-Release: %{release}
-License: SugarCRM Licence
-Group: Development/Tools
-Source: %{name}-%{version}.tgz
-URL: https://github.com/sugarcrm/xhprof-viewer
-BuildRoot: %{buildroot}
-BuildArch: noarch
-
-%description
-%{summary}
-
-%prep
-%setup -q
-
-%build
-# Nothing to build
-
-%install
-mkdir -p \$RPM_BUILD_ROOT/var/www/html/xhp
-cp -R ./* \$RPM_BUILD_ROOT/var/www/html/xhp
-
-%files
-/var/www/html/xhp
-
-EOF
-
-rpmbuild -bb --target=noarch --target=noarch-unknown-linux SPECS/$package_name.spec
-mv RPMS/noarch/$package_name-1.noarch.rpm ../../
-
-cd ../..
-rm -r dist
-ls $package_name.tgz
-ls $package_name-1.noarch.rpm
+tar -czf "../$archive_name.tar.gz" $package_name
+zip -qr "../$archive_name.zip" $package_name
